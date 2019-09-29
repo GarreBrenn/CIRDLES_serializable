6b841218bc015bac3f5942797078eb42bea43962
Garrett Brenner <garrebrenna@gmail.com>
2019-09-29 12:57:08 -0400

    implemented serializing to .txt

---
+++ src/main/java/Driver.java
@@ -0,0 +1,32 @@
+import java.io.IOException;
+
+public class Driver {
+    private String filename = "studentinfo.txt";
+
+    public void run() {
+        Student stud = new Student("Garrett","Brenner","20131189");
+
+        print("stud serializing...");
+        try {
+            Student.serialize(stud, filename);
+        }
+        catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+
+        print("stud2 deserializing...");
+        Student stud2 = Student.deserialize(filename);
+
+        print("comparing...");
+        if (stud.compareTo(stud2) == 0) {
+            print("The students are equal");
+        }
+        else {
+            print("The students are not equal");
+        }
+    }
+
+    private void print(String s) {
+        System.out.println(s);
+    }
+}
